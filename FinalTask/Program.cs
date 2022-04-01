f58d32b1b492ed288bd0244c5786614be34cff75
IvanKurylev93 <kuiv10@mail.ru>
2022-04-01 20:33:20 +0300

    Длина элемента массива

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,3 +1,15 @@
 ﻿Console.Clear();
 string[] array1 = new string[5] {"478", "for", "hell", "world", "peace"};
 string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
